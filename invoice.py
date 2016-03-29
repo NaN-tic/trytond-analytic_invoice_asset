b95303d58e11dd0c54eb84ec949b90d8bd91754a
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-03-29 14:35:22 +0200

    Implement functionality

---
+++ invoice.py
@@ -0,0 +1,23 @@
+# The COPYRIGHT file at the top level of  this repository contains the full
+# copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta, Pool
+
+__all__ = ['InvoiceLine']
+
+
+class InvoiceLine:
+    __name__ = 'account.invoice.line'
+    __metaclass__ = PoolMeta
+
+    @fields.depends('invoice_asset', 'analytic_accounts')
+    def on_change_invoice_asset(self):
+        pool = Pool()
+        Entry = pool.get('analytic.account.entry')
+        if self.invoice_asset:
+            entries = []
+            for entry in self.invoice_asset.analytic_accounts:
+                new_entry = Entry(root=entry.root, account=entry.account)
+                new_entry.required = new_entry.on_change_with_required()
+                entries.append(new_entry)
+            self.analytic_accounts = entries
